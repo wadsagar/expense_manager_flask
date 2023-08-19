d24fbcd00c151e4cc610f9be07f0e0d9622d4e9a
wadsagar <111647257+wadsagar@users.noreply.github.com>
2023-08-19 18:40:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app=Flask(__name__)
 
 
-@app.route("/add")
+@app.route("/")
 def add():
     return render_template('add.html')
 
-@app.errorhandler(404)
-def invalid():
-    return render_template('add.html')
+# @app.errorhandler('/404')
+# def invalid():
+#     return render_template('add.html')
+
+@add.route('/addexpense', method=['POST'])
+def addexpense():
+    date = request.form['date']
+    name = request.form['enxpensename']
+    amount = request.form['amount']
+    category = request.form['catagory']
+    print(date + " " + name + ' ' + amount + ' '  + category)
+    return redirect("/")
 
 
 if __name__=='__main__':
